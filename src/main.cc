dea4bb8c94e268b6638f1b4e071b8d92d92a42fe
Azusain <azusaings@gmail.com>
2023-12-26 07:11:14 +0000

    demo cpu

--- src/main.cc
+++ src/main.cc
@@ -5,24 +5,26 @@
 
 int main(int argc, char* argv[]) {
   cv::Mat img = cv::imread("../data/images/bus.jpg");
-  auto height = img.rows;
-  auto width = img.cols;
-
+  
   DetectionModelYolov8 m{"../data/models/yolov8m.onnx"};
   auto results = m.Predict(img);
-  fmt::println("read image ({}, {})", height, width);
+  
+  // draw rectangles
+  auto height = img.rows;
+  auto width = img.cols;
   for(auto& [k, v] : results) {
-    fmt::println("+-------- id: {} --------+", k);
     for(auto& res : v) {
-      fmt::println(
-        "xywh: {}, {}, {}, {}", 
+      cv::Rect rect(
         res.left * width, 
         res.top * height, 
         res.width * width, 
         res.height * height
       );
+      cv::rectangle(img, rect, cv::Scalar(0, 240, 0), 2);
     }
   }
+
+  cv::imwrite("output.jpg", img);
   return 0;
 }
 
